c006e8dc415c8d3d37a3ac887bb55bd623c86124
rviktor87 <rviktor87@web-fejlesztes.eu>
2023-06-13 10:37:25 +0200

    Updating backend exam

--- assets/tinder_clone.sql
+++ assets/tinder_clone.sql
@@ -1,27 +1,21 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MariaDB
+ Source Server Version : 101102 (10.11.2-MariaDB)
+ Source Host           : localhost:3306
+ Source Schema         : tinder_clone
+
+ Target Server Type    : MariaDB
+ Target Server Version : 101102 (10.11.2-MariaDB)
+ File Encoding         : 65001
+
+ Date: 13/06/2023 10:29:39
+*/
+
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for users
--- ----------------------------
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-                       `id` int(11) NOT NULL AUTO_INCREMENT,
-                       `email` varchar(255) NOT NULL,
-                       `birth_date` date NOT NULL,
-                       `gender` varchar(255) NOT NULL,
-                       `bio` text DEFAULT NULL,
-                       PRIMARY KEY (`id`),
-                       UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- ----------------------------
--- Records of users
--- ----------------------------
-BEGIN;
-INSERT INTO `users` (`id`, `email`, `birth_date`, `gender`, `bio`) VALUES (1, 'mary@pure.com', '2000-07-06', 'female', 'I\'m a virgin, I\'m not sure how I got here.');
-INSERT INTO `users` (`id`, `email`, `birth_date`, `gender`, `bio`) VALUES (2, 'john@pure.com', '1998-01-24', 'male', 'I\'m a smith, John Smith.');
-COMMIT;
 
 -- ----------------------------
 -- Table structure for swipes
@@ -44,6 +38,29 @@ CREATE TABLE `swipes` (
 -- ----------------------------
 BEGIN;
 INSERT INTO `swipes` (`id`, `current_user_id`, `other_user_id`, `direction`) VALUES (1, 2, 1, 'right');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `birth_date` date NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `bio` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+BEGIN;
+INSERT INTO `users` (`id`, `name`, `email`, `birth_date`, `gender`, `bio`) VALUES (1, 'Mary', 'mary@pure.com', '2000-07-06', 'female', 'I\'m a virgin, I\'m not sure how I got here.');
+INSERT INTO `users` (`id`, `name`, `email`, `birth_date`, `gender`, `bio`) VALUES (2, 'John', 'john@pure.com', '1998-01-24', 'male', 'I\'m a smith, John Smith.');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
