79a35ed6687b905e801843164b6899302d7a4747
luca-trnka <lucie-trnkova76@seznam.cz>
2023-06-16 09:34:12 +0200

    project created, properties and build.gradle set

---
+++ TinderClone/src/main/java/com/gfa/tinderclone/TinderCloneApplication.java
@@ -0,0 +1,13 @@
+package com.gfa.tinderclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderCloneApplication.class, args);
+	}
+
+}
